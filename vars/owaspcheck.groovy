60439195b92dea9c6aa983abfffd1dbd718d9950
Vasu Antala <vasuantala098@gmail.com>
2026-03-12 00:32:18 +0530

    Add NVD API key to OWASP check function

--- vars/owaspcheck.groovy
+++ vars/owaspcheck.groovy
@@ -1,4 +1,6 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_KEY')]) {
+        dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${NVD_KEY}", odcInstallation: 'OWASP'
+        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    }
 }
